cc0e410c68b595bcbb53fdac874785086bfcca4b
feng <908087098@qq.com>
2019-05-13 16:59:37 +0800

    新增 起始事件组件

---
+++ src/components/events/EventStart.ts
@@ -0,0 +1,22 @@
+/**
+ * 事件起始
+ */
+class EventStart extends eui.Component implements eui.UIComponent
+{
+	public constructor()
+	{
+		super();
+	}
+
+	protected partAdded(partName: string, instance: any): void
+	{
+		super.partAdded(partName, instance);
+	}
+
+
+	protected childrenCreated(): void
+	{
+		super.childrenCreated();
+	}
+
+}
